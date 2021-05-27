0ab258e133f55a59e1864745277d6e5ee16196ee
Chandandeep67 <duttaarnab71@gmail.com>
2021-05-27 21:39:53 +0530

    initial-commit

---
+++ src/main/java/com/example/springjpatuto/SpringJpaTutoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springjpatuto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringJpaTutoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringJpaTutoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/springjpatuto/SpringJpaTutoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springjpatuto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJpaTutoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
